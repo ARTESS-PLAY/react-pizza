bc680ae452e1a9e05e9a8fd6b8526eddec2f3aae
Artem <artess.ponomarev@mail.ru>
2023-01-31 19:15:05 +0300

    added pizza placeholder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
+
+import axios from 'axios';
+
 import Header from './components/Header/Index';
 import PizzaCard from './components/PizzaCard';
+import Placeholder from './components/PizzaCard/Placeholder';
 import Categoties from './components/Categories';
 import Sort from './components/Sort';
 
-import pizzas from './assets/pizza.json';
-
 function App() {
+    const [items, setItems] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        axios
+            .get('http://localhost:3005/items/')
+            .then((res) => setItems(res.data))
+            .finally(() => setIsLoading(false));
+    }, []);
     return (
         <div className="wrapper">
             <Header></Header>
@@ -17,9 +29,11 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((el) => (
-                            <PizzaCard key={el.id} {...el} />
-                        ))}
+                        {isLoading
+                            ? Array(10)
+                                  .fill(0)
+                                  .map((el, i) => <Placeholder key={i} />)
+                            : items.map((el) => <PizzaCard key={el.id} {...el} />)}
                     </div>
                 </div>
             </div>
---
+++ src/components/PizzaCard/Placeholder.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Placeholder = (props) => (
+    <ContentLoader
+        speed={2}
+        width={280}
+        height={466}
+        viewBox="0 0 280 466"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+        {...props}>
+        <circle cx="140" cy="125" r="125" />
+        <rect x="0" y="265" rx="15" ry="15" width="280" height="20" />
+        <rect x="0" y="293" rx="15" ry="15" width="280" height="88" />
+        <rect x="0" y="405" rx="15" ry="15" width="90" height="27" />
+        <rect x="128" y="392" rx="30" ry="30" width="152" height="45" />
+    </ContentLoader>
+);
+
+export default Placeholder;
