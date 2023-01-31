bc4888f9f8083e7f93e4aa3320e95a86aa205763
Artem <artess.ponomarev@mail.ru>
2023-01-31 17:11:18 +0300

    list render

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import './App.css';
 import Header from './components/Header/Index';
 import PizzaCard from './components/PizzaCard';
 import Categoties from './components/Categories';
 import Sort from './components/Sort';
 
+import pizzas from './assets/pizza.json';
+
 function App() {
-  return (
-    <div className="wrapper">
-      <Header></Header>
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categoties></Categoties>
-            <Sort></Sort>
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            <PizzaCard></PizzaCard>
-            <PizzaCard></PizzaCard>
-            <PizzaCard></PizzaCard>
-            <PizzaCard></PizzaCard>
-            <PizzaCard></PizzaCard>
-            <PizzaCard></PizzaCard>
-            <PizzaCard></PizzaCard>
-            <PizzaCard></PizzaCard>
-            <PizzaCard></PizzaCard>
-          </div>
+    return (
+        <div className="wrapper">
+            <Header></Header>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categoties></Categoties>
+                        <Sort></Sort>
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {pizzas.map((el) => (
+                            <PizzaCard key={el.id} {...el} />
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,16 +1,27 @@
+import React from 'react';
+
 function Categoties() {
-  return (
-    <div className="categories">
-      <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
-      </ul>
-    </div>
-  );
+    const [activeIndex, setActiveIndex] = React.useState(0);
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index);
+    };
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map((el, index) => (
+                    <li
+                        key={index}
+                        className={activeIndex === index ? 'active' : ''}
+                        onClick={() => onClickCategory(index)}>
+                        {el}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default Categoties;
