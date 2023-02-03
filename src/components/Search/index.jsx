48979dcfaba281cc23f5ad26f85bb8871abf0a9f
Artem <artess.ponomarev@mail.ru>
2023-02-03 18:06:38 +0300

    used ref to focus input search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,15 +4,24 @@ import cl from './Search.module.scss';
 
 function Search() {
     const { search, setSearch } = React.useContext(SearchContext);
+    const inputRef = React.useRef();
     return (
         <div className={cl.search}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M3.624,15a8.03,8.03,0,0,0,10.619.659l5.318,5.318a1,1,0,0,0,1.414-1.414l-5.318-5.318A8.04,8.04,0,0,0,3.624,3.624,8.042,8.042,0,0,0,3.624,15Zm1.414-9.96a6.043,6.043,0,1,1-1.77,4.274A6,6,0,0,1,5.038,5.038Z" />
             </svg>
-            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+            <input
+                type="text"
+                ref={inputRef}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
             {search && (
                 <svg
-                    onClick={() => setSearch('')}
+                    onClick={() => {
+                        setSearch('');
+                        inputRef.current.focus();
+                    }}
                     className={cl.close}
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24">
