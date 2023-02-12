b89105c372b164d43fbd4d078b1922bdcc2f13be
Artem <artess.ponomarev@mail.ru>
2023-02-12 16:33:25 +0300

    added react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import SearchContext from './context/SearchContext';
 
 import Home from './Pages/Home';
 import Header from './components/Header/Index';
-import NotFound from './Pages/NotFound';
-import Cart from './Pages/Cart';
-import PizzaPage from './Pages/PizzaPage';
+
+const NotFound = React.lazy(() => import(/*webpachChunkName: "NotFound" */ './Pages/NotFound'));
+const Cart = React.lazy(() => import(/*webpachChunkName: "Cart" */ './Pages/Cart'));
+const PizzaPage = React.lazy(() => import(/*webpachChunkName: "PizzaPage" */ './Pages/PizzaPage'));
 
 function App() {
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/pizza/:id" element={<PizzaPage />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
+                <Suspense fallback={<h2>Идёт загрузка...</h2>}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/pizza/:id" element={<PizzaPage />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Suspense>
             </div>
         </div>
     );
